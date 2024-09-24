d504ead881b279f9fdcf978459bcf3e2f0f54bc3
Lucas Bento <164066753+LcBento@users.noreply.github.com>
2024-09-24 16:47:16 -0300

    separei em pastas

--- ex016/script-ex016.js
+++ ex016/script-ex016.js
@@ -8,15 +8,15 @@ function carregar() {
 
     if (hora >= 0 && hora < 12) {
         mensage.innerHTML = `Agora são ${hora}h e ${minute}m Bom Dia!.`
-        img.src = 'imagens/1.foto.manha.png'
+        img.src = 'images/1.foto.manha.png'
         document.body.style.background = 'rgb(233, 216, 186)'
     } else if (hora >= 12 && hora <= 18) {
         mensage.innerHTML = `Agora são ${hora}h e ${minute}m Boa Tarde!.`
-        img.src = 'imagens/2.foto.tarde.png'
+        img.src = 'images/2.foto.tarde.png'
         document.body.style.background = 'rgb(139, 143, 116)'
     } else {
         mensage.innerHTML = `Agora são ${hora}h e ${minute}m Boa Noite!.`
-        img.src = 'imagens/3.foto.noite.png'
+        img.src = 'images/3.foto.noite.png'
         document.body.style.background = 'rgb(93, 87, 75)'
     }
 }
