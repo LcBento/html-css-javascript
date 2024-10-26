31eb6b507ce334e77869a2b97ec3249632c9853c
Lucas Bento <164066753+LcBento@users.noreply.github.com>
2024-10-26 11:42:42 -0300

    adicionei aulas

--- ex052/script-ex052.js
+++ ex052/script-ex052.js
@@ -1,11 +1,29 @@
 function contar() {
-    var ini = docuemnt.getElementById('txti')
-    var fim = docuemnt.getElementById('txtf')
-    var passo = docuemnt.getElementById('txtp')
+    let inicio = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam Dados!')
     } else {
-        alert('TUDO OK!')
+        res.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (i < f) {
+            //contagem crescente
+            for(let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} \u{1F449}`
+            } 
+        } else {
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F448}`
+            }
+        }
+        
+        res.innerHTML += `\u{1F3C1}`
     }
 }
