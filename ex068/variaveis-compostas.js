a172fa9624a73e1c4f278de5ecdb9ec350e007fc
Lucas Bento <164066753+LcBento@users.noreply.github.com>
2024-10-26 14:21:11 -0300

    acicionei mais uma aula

---
+++ ex068/variaveis-compostas.js
@@ -0,0 +1,10 @@
+let num = [3, 4, 1, 2]
+
+num[4] = 5   //adiciona o número na posição 4
+num.push(6)  //adiciona o número na última posição
+num.length   //para mostrar quantos elemento
+num.sort()   //coloca os números em ordem crescente
+
+console.log(`os vetores são ` + num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O vetor na posição 0 é ` + num[0])
