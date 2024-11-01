93e8437583296b59b185a7373759408bab39b395
Lucas Bento <164066753+LcBento@users.noreply.github.com>
2024-11-01 10:36:36 -0300

    adicionei observações a aula

--- ex068/variaveis-compostas.js
+++ ex068/variaveis-compostas.js
@@ -5,6 +5,10 @@ num.push(6)  //adiciona o número na última posição
 num.length   //para mostrar quantos elemento
 num.sort()   //coloca os números em ordem crescente
 
+//num é uma variável composta/array/vetor, que é composto de elementos, 
+//o elemento de um vetor é um par que agrupa o espaço da memória, o valor colocado dentro dele e o índice.
+//índice/chave/key, é a posição de cada conteúdo.
+
 console.log(`os vetores são ` + num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O vetor na posição 0 é ` + num[0])
